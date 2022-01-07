8692087a91d54c4710e224dea97c24c7577845c2
Russ Carroll <info@rcarroll.dev>
2022-01-06 21:43:49 -0500

    Broke down components and style menu-item.

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -2,12 +2,19 @@ import React from 'react';
 
 import './menu-item.component.scss'
 
-const MenuItem = ({ title }) => (
-  <div className="menu-item">
-    <div className="content">
-      <h1 className="title">{title}</h1>
-      <span className="subtitle">shop now</span>
-    </div>
+const MenuItem = ({ title, imageUrl, size }) => (
+  <div 
+  className={`${size} menu-item`}>
+    <div 
+      className="background-image" 
+      style={{
+        backgroundImage: `url(${imageUrl})`
+      }} />
+      <div className="content">
+        <h1 className="title">{title.toUpperCase()}</h1>
+        <span className="subtitle">shop now</span>
+      </div>
+    
   </div>
 );
 
